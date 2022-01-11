9043272663a1179d8dce99017ec8f641da8ed899
Joel Turner <joel_turner@wiwd.uscourts.gov>
2022-01-11 10:50:18 -0600

    updates

---
+++ Linear Algebra/Exercise.py
@@ -0,0 +1,13 @@
+Vectors
+
+#2
+import matplotlib.pyplot as plt
+import numpy as np
+
+# This code draws the x and y axis as lines.
+
+# plt.axhline(0, c='black', lw=0.5)
+# plt.axvline(0, c='black', lw=0.5)
+# plt.xlim(-3,3)
+# plt.ylim(-4,4)
+
